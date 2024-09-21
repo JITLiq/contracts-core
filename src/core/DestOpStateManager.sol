af52027715dc74c5a9ac1dfc335a67b3437d4ddb
pradeep-selva <pradeepselvaraju@gmail.com>
2024-09-21 08:27:33 +0800

    ft: add base token to destSM

--- src/core/DestOpStateManager.sol
+++ src/core/DestOpStateManager.sol
@@ -5,10 +5,18 @@ import {IDestOpStateManager} from "src/interfaces/IDestOpStateManager.sol";
 import {AddressRegistryService} from "src/core/AddressRegistryService.sol";
 
 contract DestOpStateManager is IDestOpStateManager, AddressRegistryService {
-    address[] operators;
-    uint256 totalFundsOnHold;
+    address internal immutable _BASE_BRIDGE_TOKEN;
 
-    constructor(address _addressRegistry) AddressRegistryService(_addressRegistry) {}
+    address[] public operators;
+    uint256 public totalFundsOnHold;
+
+    constructor(address _addressRegistry, address _baseBridgeToken) AddressRegistryService(_addressRegistry) {
+        _BASE_BRIDGE_TOKEN = _baseBridgeToken;
+    }
+
+    function baseBridgeToken() external view returns (address) {
+        return _BASE_BRIDGE_TOKEN;
+    }
 
     function syncSourceData(address[] memory newOperators, uint256 newTotalFundsOnHold) external {
         operators = newOperators;
--- src/interfaces/IDestOpStateManager.sol
+++ src/interfaces/IDestOpStateManager.sol
@@ -2,5 +2,7 @@
 pragma solidity ^0.8.19;
 
 interface IDestOpStateManager {
+    function baseBridgeToken() external view returns (address);
+
     function syncSourceData(address[] memory operators, uint256 totalFundsOnHold) external;
 }
